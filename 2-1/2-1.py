ec69e6bb35a8d8225778a85ec34a337d45dda750
tomas <tomas.kozakas@gmail.com>
2023-09-14 16:40:13 +0300

    2nd pratybos 1 task

---
+++ 2-1/2-1.py
@@ -0,0 +1,84 @@
+from math import gcd
+
+ciphertext = "EZĘKU ĘERKR EŠEIE MREŠE IEMEK RZĘČL ĮNELA TVMBI MKUAM ELĘNŪ ŠŪPTR EIHYP AELUP MCIMŪ KCŪŽM UAKŽM CMŪĖK UCEPM NYĮDP CUAEŽ HEŠUĘ EAJMI VHEMA VNŪPP REMGM KUĮEĖ ŪŽKUĮ MĮŪML ŪĘMRM CEĘPK VŽMCR MICEĖ EĘČPM"
+text_start = "AŠT"
+
+abc = u'AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ'
+n = len(abc)
+
+
+def affine_cipher(text, a, b, mode="encrypt"):
+    """
+    Encrypts or decrypts a given text using the affine cipher.
+
+    Parameters:
+    - text: The text to be encrypted/decrypted.
+    - a: The 'a' key for the affine cipher.
+    - b: The 'b' key for the affine cipher.
+    - mode: Either "encrypt" or "decrypt".
+
+    Returns:
+    - The encrypted or decrypted text.
+    """
+    text_u = text.upper()
+    t = u''
+    for r in text_u:
+        if r in abc:
+            t += r
+    c = u''
+
+    # Check if 'a' is coprime to length of alphabet
+    if gcd(a, n) != 1:
+        raise ValueError(f"'a' value {a} is not coprime to {n}.")
+
+    # Encrypt or decrypt based on mode
+    for r in t:
+        if mode == "encrypt":
+            m = (a * abc.index(r) + b) % n
+        elif mode == "decrypt":
+            a_inv = mod_inverse(a, n)  # Multiplicative inverse of 'a'
+            m = (a_inv * (abc.index(r) - b)) % n
+        c += abc[m]
+
+    return c
+
+
+def brute_force_affine_to_find_start(ciphertext, desired_start):
+    """
+    Brute-forces the affine cipher using all possible combinations of 'a' and 'b'
+    until a decryption is found that starts with the desired start text.
+
+    Parameters:
+    - ciphertext: The encrypted text.
+    - desired_start: The desired starting characters of the decrypted text.
+
+    Returns:
+    - The decrypted text, a, and b values if found. Otherwise, None.
+    """
+
+    possible_a_values = coprime_list(34)
+    for a in possible_a_values:
+        for b in range(n):
+            decrypted_text = affine_cipher(ciphertext, a, b, mode="decrypt")
+            if decrypted_text.startswith(desired_start):
+                return decrypted_text, a, b
+    return None, None, None
+
+
+def coprime_list(n):
+    """Return a list of numbers from 1 to n that are coprime to n."""
+    return [i for i in range(1, n + 1) if gcd(i, n) == 1]
+
+
+def mod_inverse(a, m):
+    """
+    Calculate the modular inverse of a modulo m.
+    """
+    for x in range(1, m):
+        if (a * x) % m == 1:
+            return x
+    raise ValueError(f"Modular inverse for {a} mod {m} does not exist")
+
+
+decrypted_text, found_a, found_b = brute_force_affine_to_find_start(ciphertext, text_start)
+print(decrypted_text)
